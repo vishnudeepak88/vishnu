90477b197d290674fc608603efa4d98fafb6c066
vishnudeepak88 <vishnu.deepak@lnttechservice.com>
2016-11-29 14:46:33 +0530

    my_code

---
+++ text.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+ char a[20] = "vishnu_deepak";
+ char *p;
+ p = (char *)malloc(sizeof(a));
+ p = (char *) &a;
+
+ printf("%s\r\n",(char *)p);
+
+// free(p);
+ return 0;
+}
